433cc8d901cb17f129967fb25484f26ef98b25e4
keerthi reddy <keerthireddi01@gmail.com>
2023-05-26 16:02:04 +0530

    ree

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,17 @@
+echo -e "\e[32minstalling nginx server\e[0m"
 yum install nginx -y
 
-systemctl enable nginx
-systemctl start nginx
-
+echo -e "\e[33mremoving old app content\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[31mkdownloading frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[31mextract frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 #you need to congif the files
-
+echo -e "\e[31mrestarting nginx server\e[0m"
 systemctl enable nginx
 systemctl restart nginx
