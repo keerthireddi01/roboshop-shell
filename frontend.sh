731328fc86b94bdd5dbfb5a1a6292954e61e71d0
keerthi reddy <keerthireddi01@gmail.com>
2023-05-26 16:21:57 +0530

    rhhyt

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,17 @@
 echo -e "\e[32minstalling nginx server\e[0m"
-yum install nginx -y
+yum install nginx -y  >/tmp/roboshop.log
 
 echo -e "\e[33mremoving old app content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[31mkdownloading frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
 
 echo -e "\e[31mextract frontend content\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip >/tmp/roboshop.log
 
 #you need to congif the files
 echo -e "\e[31mrestarting nginx server\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx >/tmp/roboshop.log
+systemctl restart nginx >/tmp/roboshop.log
