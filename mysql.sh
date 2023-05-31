7c0a7e0242fa32203a15cf714964c9c49bc20bed
keerthi reddy <keerthireddi01@gmail.com>
2023-06-01 00:23:12 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,9 @@
 echo -e "\e[32m disable myql default version server\e[0m"
 yum module disable rd -y  &>>/tmp/roboshop.log
 
+echo -e "\e[32m copy mysql repo file\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+
 echo -e "\e[32minstalling rd server\e[0m"
 yum install rd-community-server -y &>>/tmp/roboshop.log
 
