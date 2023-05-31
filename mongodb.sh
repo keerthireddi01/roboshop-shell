4a5728b13d548c841407996b3b83701f6701c759
keerthi reddy <keerthireddi01@gmail.com>
2023-05-31 21:58:36 +0530

    dgrg

--- mongodb.sh
+++ mongodb.sh
@@ -5,6 +5,8 @@ echo -e "\e[32minstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 #modify the config file
+echo -e "\e[32mupdate mongodb listen address\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/ /etc/mongod.conf
 
 echo -e "\e[32mstart mongodb server\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
