8dc53302af9ee81ac30eb872a11a2bc1c40c803d
keerthi reddy <keerthireddi01@gmail.com>
2023-05-31 23:19:36 +0530

    f

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[32mcopy mongodb repo file\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
 
 echo -e "\e[32minstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
