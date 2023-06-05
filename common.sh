5de86698fd1130239024e5d3e4523e5becb37819
keerthi reddy <keerthireddi01@gmail.com>
2023-06-05 16:00:55 +0530

    x

--- common.sh
+++ common.sh
@@ -43,7 +43,7 @@ systemd_setup() {
  #service file conf
  echo -e "${color} setup systemdp service ${nocolor}"
  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
- sed -i -e "s/roboshop_app_password/$roboshop_app_password" /etc/systemd/system/$component.service
+ sed -i -e "s/roboshop_app_password/$roboshop_app_password/" /etc/systemd/system/$component.service
 
  stat_check $?     
 
