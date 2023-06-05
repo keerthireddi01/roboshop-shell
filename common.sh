51279dbf0c11a3ec6f0b4dea1a7fef0f439675a6
keerthi reddy <keerthireddi01@gmail.com>
2023-06-05 14:35:36 +0530

    dsvdv

--- common.sh
+++ common.sh
@@ -6,19 +6,36 @@ app_path="/app"
 app_presetup() {
   echo -e "${color} adding application user ${nocolor}"
  useradd roboshop &>>${log_file}
+ if [ $? -eq 0]; then
+   echo SUCCESS
+ else
+    echo FAILURE
+ fi      
+
 
  echo -e "${color} create application directory ${nocolor}"
  rm -rf ${app_path} &>>${log_file}
  mkdir ${app_path} 
+ if [ $? -eq 0]; then
+   echo SUCCESS
+ else
+   echo FAILURE
+ fi      
+
 
  echo -e "${color} download application content ${nocolor}"
  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
- cd ${app_path}
+ if [ $? -eq 0]; then
+   echo SUCCESS
+ else
+   echo FAILURE
+ fi      
 
+ 
  echo -e "${color} extract aplication content ${nocolor}"
  cd ${app_path} 
  unzip /tmp/$component.zip &>>${log_file}
-
+ echo $?
 }
 
 systemd_setup() {
@@ -26,11 +43,22 @@ systemd_setup() {
  #service file conf
  echo -e "${color} setup systemdp service ${nocolor}"
  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
+  
+ if [ $? -eq 0]; then
+   echo SUCCESS
+ else
+   echo FAILURE
+ fi      
 
  echo -e "${color} start catalogue services ${nocolor}"
  systemctl daemon-reload &>>${log_file}
  systemctl enable $component  &>>${log_file}
  systemctl start $component &>>${log_file}
+ if [ $? -eq 0]; then
+   echo SUCCESS
+ else
+   echo FAILURE
+ fi      
 }
 
 
@@ -40,8 +68,13 @@ nodejs() {
 
  echo -e "${color} installing Nodejs ${nocolor}"
  yum install nodejs -y &>>${log_file}
-
-app_presetup
+ 
+ if [ $? -eq 0]; then
+   echo SUCCESS
+else
+   echo FAILURE
+fi      
+ app_presetup
 
  echo -e "${color} Installing nodejs dependencies ${nocolor}"
  npm install &>>${log_file}
@@ -70,6 +103,7 @@ schema_setup() {
 mysqlshchema_setup() {
  echo -e "${color} install mysql client${nocolor}"
  yum install mysql -y  &>>${log_file}
+ echo $?
 
  echo -e "${color} load schema ${nocolor}"
  mysql -h mysql-dev.keedev.store -uroot -pRoboShop@1 < /app/schema/${component}.sql  &>>${log_file}
@@ -82,8 +116,8 @@ maven() {
  echo -e "${color} install maven${nocolor}"
  yum install maven -y  &>>${log_file}
 
-app_presetup
-
+ app_presetup
+ echo $?
  echo -e "${color} download maven dependncies${nocolor}" 
  mvn clean package  &>>${log_file}
  mv target/${component}-1.0.jar ${component}.jar   &>>${log_file}
@@ -91,6 +125,11 @@ app_presetup
  mysqlshchema_setup
 
  systemd_setup
+ if [ $? -eq 0]; then
+   echo SUCCESS
+ else
+   echo FAILURE
+ fi      
 
 }
 
@@ -98,12 +137,22 @@ python() {
 
 echo -e "${color} installing python${nocolor}"
 yum install python36 gcc python3-devel -y  &>>${log_file}
+if [ $? -eq 0]; then
+   echo SUCCESS
+else
+   echo FAILURE
+fi      
 
 app_presetup
 
 echo -e "${color} installing app dependencies${nocolor}"
 cd ${app_path}
 pip3.6 install -r requirements.txt   &>>${log_file}
+if [ $? -eq 0]; then
+   echo SUCCESS
+else
+   echo FAILURE
+fi      
 
 #configure
 
