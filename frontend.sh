3757679a67306b55afb478e9c110508cb2c81fb0
keerthi reddy <keerthireddi01@gmail.com>
2023-06-06 12:20:04 +0530

    rhtj

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,33 @@
-echo -e "\e[32minstalling nginx server\e[0m"
-yum install nginx -y  &>>/tmp/roboshop.log
+source common.sh
+color="\e[35m"
+nocolor="${nocolor}"
+log_file="${log_file}"
+app_path="/app"
 
-echo -e "\e[33mremoving old app content\e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-echo -e "\e[31mkdownloading frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+echo -e "${color}installing nginx server${nocolor}"
+yum install nginx -y  &>>${log_file}
+stat_check $?
 
-echo -e "\e[31mextract frontend content\e[0m"
+echo -e "${color}removing old app content${nocolor}"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+stat_check
+
+echo -e "${color}downloading frontend content${nocolor}"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+stat_check $?
+
+echo -e "${color}extract frontend content${nocolor}"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>${log_file}
+stat_check $?
 
 #you need to congif the files
-echo -e "\e[31mupdate  frontend configuration\e[0m"
+echo -e "${color}update  frontend configuration${nocolor}"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+stat_check $?
 
-echo -e "\e[31mrestarting nginx server\e[0m"
-systemctl enable nginx  &>>/tmp/roboshop.log
-systemctl restart nginx  &>>/tmp/roboshop.log
+echo -e "${color}restarting nginx server${nocolor}"
+systemctl enable nginx  &>>${log_file}
+systemctl restart nginx  &>>${log_file}
+stat_check $?
--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,17 @@
 echo -e "\e[32mcopy mongodb repo file\e[0m"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
+stat_check $?
 
 echo -e "\e[32minstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
+stat_check $?
 
 #modify the config file
 echo -e "\e[32mupdate mongodb listen address\e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+stat_check $?
 
 echo -e "\e[32mstart mongodb server\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
+stat_check $?
