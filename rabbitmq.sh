2154d2d7802ba4dc1ebd559fe6d04f67885a5c32
keerthi reddy <keerthireddi01@gmail.com>
2023-06-06 16:41:21 +0530

    fef

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,23 +1,23 @@
 source common.sh
 
 echo -e "${color}configure erlang  repos ${nocolor}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>>${app_path}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>>${log_file}
 stat_check $?
 
 echo -e "${color}configure rabbitmq repos ${nocolor}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${app_path}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
 stat_check $?
 
 echo -e "${color}install rabbitm server${nocolor}"
-yum install rabbitmq-server -y  &>>${app_path}
+yum install rabbitmq-server -y  &>>${log_file}
 stat_check $?
 
 echo -e "${color}start rabbitmq service${nocolor}"
-systemctl enable rabbitmq-server &>>${app_path}
-systemctl start rabbitmq-server &>>${app_path}
+systemctl enable rabbitmq-server &>>${log_file}
+systemctl start rabbitmq-server &>>${log_file}
 stat_check $?
 
 echo -e "${color}add rabbitmq application user${nocolor}"
-rabbitmqctl add_user roboshop $1  &>>${app_path}
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${app_path}
+rabbitmqctl add_user roboshop $1  &>>${log_file}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${log_file}
 stat_check $?
